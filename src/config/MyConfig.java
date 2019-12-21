d3a1f6af6a31f42f484965986f4ddbf4386459d1
wqmqiming <1215234059@qq.com>
2019-12-21 15:32:01 +0800

    测试提交，config初始跳转页面

---
+++ src/config/MyConfig.java
@@ -0,0 +1,14 @@
+package config;
+
+import mvc.annotation.Configuration;
+import mvc.config.ResourceHandlerRegistry;
+import mvc.config.WebConfigurer;
+
+@Configuration
+public class MyConfig extends WebConfigurer {
+
+    @Override
+    public void addViewControllers(ResourceHandlerRegistry reg) {
+        reg.addViewController("/front/front_index","/login");
+    }
+}
